52dbc3b003ef48cb53f01e93b7f2bf2d1336494e
Joel MPUNGA <joelmpunga79@gmail.com>
2024-02-05 17:07:51 +0200

    axios fixed

--- public/controller/postsController.js
+++ public/controller/postsController.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const posts = require('../model/postsModel');
 const PostsGet = (req, res) =>{
-    res.send('Hello World! Postes in controller!');
-    // posts.forEach(post => post.body)
-    res.status(200).json(posts);
-    
+    res.send('Hello World! Postes in controller!')
+    posts.map(post => post.body)
+    res.status(200).json(posts)
 }
 module.exports = PostsGet
