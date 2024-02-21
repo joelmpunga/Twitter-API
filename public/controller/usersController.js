a216c7678624e516b65dc0516cd82e4b04007efb
Joel MPUNGA <joelmpunga79@gmail.com>
2024-02-22 01:01:06 +0200

    token in cookie

--- public/controller/usersController.js
+++ public/controller/usersController.js
@@ -1,16 +1,23 @@
+const express = require('express')
+const app = express()
 const jwt = require('jsonwebtoken');
 const { getOneUser, getAllUsers, createUser } = require('../model/usersModel');
 const { json } = require('express');
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const session = require('express-session')
-const express = require('express')
 let users = getAllUsers()
 const dotenv = require('dotenv');
 dotenv.config();
 const getAll = (req, res) => {
     res.status(200).json(users)
 }
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
 
 const create = (req, res) => {
     createUser()
@@ -25,20 +32,53 @@ const getOneById = (req, res) => {
 }
 
 
-function generateTokens(username) {
-    return jwt.sign(username, process.env.TOKEN_SECRET_KEY, { expiresIn: '1800s' });
+function generateTokens(user) {
+    const payload = {
+        id: user.id,
+        username: user.username,
+        password: user.password
+    }
+    return jwt.sign(payload, process.env.TOKEN_SECRET_KEY, { expiresIn: '1800s' });
+}
+
+function verifyToken (token) {
+    return  decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY)
+}
+
+function sendToken (res,token) {
+    const options = {
+        httpOnly: true,
+        secure: true,
+        maxAge: 60*60*1000
+    };
+    res.cookie('token', token, options)
+}
+
+function getToken(req) {
+    const token = req.cookies.token;
+    return token;
 }
 
 const connexion = (req, res) => {
-    const token = generateTokens({ username: req.body.username, password: req.body.password })
-    req.session.regenerate(function (err) {
-        if (err) next(err)
-        req.session.user = req.body.user
-        req.session.save(function (err) {
-            if (err) return next(err)
-            res.status(200).json(req.session.user +" est le token est "+token)
-        })
-    })
+    const token = generateTokens({id:req.body.id, username: req.body.username, password: req.body.password })
+    const secret = process.env.TOKEN_SECRET_KEY;
+    const payload = jwt.verify(token, secret)
+    console.log(payload.password, password, payload.username, username);
+    if (payload.password === password || payload.username === username) {
+        res.status(200).json(token)
+    }
+    else {
+        res.status(401).json("Bad Request")
+    }
+    // req.session.regenerate(function (err) {
+    //     if (err) next(err)
+    //     req.session.user = req.body.user
+    //     req.session.save(function (err) {
+    //         if (err) return next(err)
+    //         res.status(200).json(req.session.user + " est le token est " + token)
+    //     })
+    // })
+    res.status(200).json(token)
 }
 
 const authenticateToken = (req, res, next) => {
